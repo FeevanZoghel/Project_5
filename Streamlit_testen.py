685409af6df103c2636dd9e77b96d9b1af621812
FeevanZoghel <582755@student.fontys.nl>
2026-09-10 10:30:28 +0200

    Create Streamlit_testen.py

---
+++ Streamlit_testen.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Mijn eerste app")
+st.write("Hallo wereld!")
+
+st.title('Bus planning')
+bus_planning = pd.read_excel('Bus Planning.xlsx')
+st.write(bus_planning.head(10))
+
+st.title('Distance Matrix')
+distance_matrix = pd.read_excel('DistanceMatrix.xlsx')
+st.write(distance_matrix.head(10))
+
+st.title('Timetable')
+time_table = pd.read_excel('Timetable.xlsx')
+st.write(time_table.head(10))
