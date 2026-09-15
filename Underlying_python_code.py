367e7b9784aa38914be07a631cab1dc359f1e868
MatthijsvanMeeuwen <579720@student.fontys.nl>
2026-09-15 13:59:09 +0200

    Update Underlying_python_code.py

--- Underlying_python_code.py
+++ Underlying_python_code.py
@@ -1,2 +1,90 @@
-# Underlying_python_code
-print('hello')
+print('Hello World')
+#Matthijs
+
+import pandas as pd 
+
+bp = pd.read_excel('Bus_Planning.xlsx')
+# print(bp.head())
+
+dm = pd.read_excel('DistanceMatrix.xlsx')
+# print(dm.head())
+
+tt = pd.read_excel('Timetable.xlsx')
+# print(tt.head())
+
+
+total_distance = 0 
+
+start_dis           = dm['start']
+end_dis             = dm['end']
+min_travel_time     = dm['min_travel_time']
+max_travel_time     = dm['max_travel_time']
+distance_m          = dm['distance_m']
+distance_km         = distance_m/1000
+
+start_plan          = bp['start location']
+end_plan            = bp['end location']
+start_time          = bp['start time']
+end_time            = bp['end time']
+activity            = bp['activity']
+line                = bp['line']
+energy_consumption  = bp['energy consumption']
+bus_number          = bp['bus'].unique()
+
+start_battery       = 300 # start waarde van 85% (gaan uit van het minimum)
+min_waarde_battery  = (300/85 *100)*.1 # 10 % van de echte waarde aanwezig zijn
+planning_sor        = bp.sort_values(['bus','start time']).reset_index(drop =True) #sorteerd per bus, per begintijd op chronologische volgorde
+
+for bus, bus_data in planning_sor.groupby('bus'):
+    bus_data = bus_data.reset_index(drop=True)
+
+    for i in range(len(bus_data)):
+        if i==len(bus_data)-1:
+                    break
+        if bus_data['end time'][i]>bus_data['start time'][i+1]:
+            print(f'Overlap gevonden bij bus {bus}')
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
