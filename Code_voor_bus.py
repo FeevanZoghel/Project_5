56af949e5ce5e369862aca2e40742cb4d2ac79bc
BasWensing <559259@student.fontys.nl>
2026-09-15 12:22:20 +0200

    Total distance

--- Code_voor_bus.py
+++ Code_voor_bus.py
@@ -153,13 +153,34 @@ d_400 = (t_ar_to_st400 * d_ar_to_st400) + (t_st_to_ar400 * d_st_to_ar400)
 d_401 = (t_ar_to_st401 * d_ar_to_st401) + (t_st_to_ar401 * d_st_to_ar401)
 
 d_service_total_m = d_400+d_401
-d_sertice_total_km = d_total_m/1000
+d_service_total_km = d_service_total_m/1000
 
 print(d_service_total_km,d_service_total_m) # Material trips moeten nog toegevoegd worden!
 
-# Nu material trips toevoegen
-d_mat_trip1 = 
-d_mat_trip2 = 
+# Nu material trips toevoegen (bussen van/uit de garage)
+# Station-Garage en andersom, Airport-Garage en andersom,
+t_bst_to_gar = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvbst') & (bp['end location'] == 'ehvgar')])
+t_gar_to_bst = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvgar') & (bp['end location'] == 'ehvbst')])
+
+t_apt_to_gar = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvapt') & (bp['end location'] == 'ehvgar')])
+t_gar_to_apt = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvgar') & (bp['end location'] == 'ehvapt')])
+
+# Bijbehorende afstanden
+d_bst_to_gar = dm[(dm['start'] == 'ehvbst') & (dm['end'] == 'ehvgar')]['distance_m'].iloc[0]  # 1650 m
+d_gar_to_bst = dm[(dm['start'] == 'ehvgar') & (dm['end'] == 'ehvbst')]['distance_m'].iloc[0]  # 1650 m
+
+d_apt_to_gar = dm[(dm['start'] == 'ehvapt') & (dm['end'] == 'ehvgar')]['distance_m'].iloc[0]  # 9000 m
+d_gar_to_apt = dm[(dm['start'] == 'ehvgar') & (dm['end'] == 'ehvapt')]['distance_m'].iloc[0]  # 9000 m
+
+# Totale afstand materials tripps
+d_material_total = (t_bst_to_gar * d_bst_to_gar) + (t_gar_to_bst * d_gar_to_bst) + (t_apt_to_gar * d_apt_to_gar) + (t_gar_to_apt * d_gar_to_apt)
+
+# Toevoegen aan de totale afstand
+total_distance_m = d_service_total_m+d_material_total
+total_distance_km = total_distance_m/1000
+
+print(total_distance_m,total_distance_km,d_material_total)
+
 
 
 
