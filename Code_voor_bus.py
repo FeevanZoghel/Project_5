2f582de9c6de455e4f8d146b6b7889e4f1896594
MatthijsvanMeeuwen <579720@student.fontys.nl>
2026-09-15 13:31:52 +0200

    Update Code_voor_bus.py

--- Code_voor_bus.py
+++ Code_voor_bus.py
@@ -34,7 +34,7 @@ bus_number          = bp['bus'].unique()
 start_battery       = 300 # start waarde van 85% (gaan uit van het minimum)
 min_waarde_battery  = (300/85 *100)*.1 # 10 % van de echte waarde aanwezig zijn
 planning_sor        = bp.sort_values(['bus','start time']) #sorteerd per bus, per begintijd op chronologische volgorde
-print(planning_sor)
+
 empty_bus           = []
 total_usage         = []
 
@@ -63,6 +63,9 @@ for bus, bus_data in planning_sor.groupby('bus'): #berekend het total verbruik p
     print(f'Bus {bus} used {total_consumption_bus:.2f} kWh')
 print(f'All busses uses a total of {total_consumption:.2f} kWh')
     
+# Berekening energiegebruik (Matthijs)
+
+# Oplaadtijd (Matthijs)
     
 
 
@@ -156,83 +159,80 @@ print(f'All busses uses a total of {total_consumption:.2f} kWh')
 
 
 
-# Bas
+# # Bas
 
 
-# Totale afstand van de bussen
+# # Totale afstand van de bussen
 
-# verplichte dienstregeling selecteren
-line_400 = dm[dm['line']==400]
-line_401 = dm[dm['line']==401]
-d_ar_to_st400 = line_400['distance_m'].iloc[0]
-d_st_to_ar400 = line_400['distance_m'].iloc[1]
+# # verplichte dienstregeling selecteren
+# line_400 = dm[dm['line']==400]
+# line_401 = dm[dm['line']==401]
+# d_ar_to_st400 = line_400['distance_m'].iloc[0]
+# d_st_to_ar400 = line_400['distance_m'].iloc[1]
 
-d_ar_to_st401 = line_401['distance_m'].iloc[0]
-d_st_to_ar401 = line_401['distance_m'].iloc[1]
+# d_ar_to_st401 = line_401['distance_m'].iloc[0]
+# d_st_to_ar401 = line_401['distance_m'].iloc[1]
 
-print(d_ar_to_st400,d_st_to_ar400,d_ar_to_st401,d_st_to_ar401)
+# print(d_ar_to_st400,d_st_to_ar400,d_ar_to_st401,d_st_to_ar401)
 
-# Verplichte dienstregeling
-# Lijn 400
-t_ar_to_st400 = len(tt[(tt['line'] == 400) & (tt['start'] == 'ehvapt') & (tt['end'] == 'ehvbst')])
-t_st_to_ar400 = len(tt[(tt['line'] == 400) & (tt['start'] == 'ehvbst') & (tt['end'] == 'ehvapt')])
+# # Verplichte dienstregeling
+# # Lijn 400
+# t_ar_to_st400 = len(tt[(tt['line'] == 400) & (tt['start'] == 'ehvapt') & (tt['end'] == 'ehvbst')])
+# t_st_to_ar400 = len(tt[(tt['line'] == 400) & (tt['start'] == 'ehvbst') & (tt['end'] == 'ehvapt')])
 
-# Lijn 401
-t_ar_to_st401 = len(tt[(tt['line'] == 401) & (tt['start'] == 'ehvapt') & (tt['end'] == 'ehvbst')])
-t_st_to_ar401 = len(tt[(tt['line'] == 401) & (tt['start'] == 'ehvbst') & (tt['end'] == 'ehvapt')])
+# # Lijn 401
+# t_ar_to_st401 = len(tt[(tt['line'] == 401) & (tt['start'] == 'ehvapt') & (tt['end'] == 'ehvbst')])
+# t_st_to_ar401 = len(tt[(tt['line'] == 401) & (tt['start'] == 'ehvbst') & (tt['end'] == 'ehvapt')])
 
-# totale afstand per lijn en totaal (van de service trips)
-d_400 = (t_ar_to_st400 * d_ar_to_st400) + (t_st_to_ar400 * d_st_to_ar400)
-d_401 = (t_ar_to_st401 * d_ar_to_st401) + (t_st_to_ar401 * d_st_to_ar401)
+# # totale afstand per lijn en totaal (van de service trips)
+# d_400 = (t_ar_to_st400 * d_ar_to_st400) + (t_st_to_ar400 * d_st_to_ar400)
+# d_401 = (t_ar_to_st401 * d_ar_to_st401) + (t_st_to_ar401 * d_st_to_ar401)
 
-d_service_total_m = d_400+d_401
-d_service_total_km = d_service_total_m/1000
+# d_service_total_m = d_400+d_401
+# d_service_total_km = d_service_total_m/1000
 
-print(d_service_total_km,d_service_total_m) # Material trips moeten nog toegevoegd worden!
+# print(d_service_total_km,d_service_total_m) # Material trips moeten nog toegevoegd worden!
 
-# Nu material trips toevoegen 
-# soorten material trips: Station-Garage en andersom, Airport-Garage en andersom,
-t_bst_to_gar = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvbst') & (bp['end location'] == 'ehvgar')])
-t_gar_to_bst = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvgar') & (bp['end location'] == 'ehvbst')])
+# # Nu material trips toevoegen 
+# # soorten material trips: Station-Garage en andersom, Airport-Garage en andersom,
+# t_bst_to_gar = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvbst') & (bp['end location'] == 'ehvgar')])
+# t_gar_to_bst = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvgar') & (bp['end location'] == 'ehvbst')])
 
-t_apt_to_gar = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvapt') & (bp['end location'] == 'ehvgar')])
-t_gar_to_apt = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvgar') & (bp['end location'] == 'ehvapt')])
+# t_apt_to_gar = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvapt') & (bp['end location'] == 'ehvgar')])
+# t_gar_to_apt = len(bp[(bp['activity'] == 'material trip') & (bp['start location'] == 'ehvgar') & (bp['end location'] == 'ehvapt')])
 
-# Afstanden van de verschillende soorten material trips
-d_bst_to_gar = dm[(dm['start'] == 'ehvbst') & (dm['end'] == 'ehvgar')]['distance_m'].iloc[0]  # 1650 m
-d_gar_to_bst = dm[(dm['start'] == 'ehvgar') & (dm['end'] == 'ehvbst')]['distance_m'].iloc[0]  # 1650 m
+# # Afstanden van de verschillende soorten material trips
+# d_bst_to_gar = dm[(dm['start'] == 'ehvbst') & (dm['end'] == 'ehvgar')]['distance_m'].iloc[0]  # 1650 m
+# d_gar_to_bst = dm[(dm['start'] == 'ehvgar') & (dm['end'] == 'ehvbst')]['distance_m'].iloc[0]  # 1650 m
 
-d_apt_to_gar = dm[(dm['start'] == 'ehvapt') & (dm['end'] == 'ehvgar')]['distance_m'].iloc[0]  # 9000 m
-d_gar_to_apt = dm[(dm['start'] == 'ehvgar') & (dm['end'] == 'ehvapt')]['distance_m'].iloc[0]  # 9000 m
+# d_apt_to_gar = dm[(dm['start'] == 'ehvapt') & (dm['end'] == 'ehvgar')]['distance_m'].iloc[0]  # 9000 m
+# d_gar_to_apt = dm[(dm['start'] == 'ehvgar') & (dm['end'] == 'ehvapt')]['distance_m'].iloc[0]  # 9000 m
 
-# Totale afstand materials tripps
-d_material_total = (t_bst_to_gar * d_bst_to_gar) + (t_gar_to_bst * d_gar_to_bst) + (t_apt_to_gar * d_apt_to_gar) + (t_gar_to_apt * d_gar_to_apt)
+# # Totale afstand materials tripps
+# d_material_total = (t_bst_to_gar * d_bst_to_gar) + (t_gar_to_bst * d_gar_to_bst) + (t_apt_to_gar * d_apt_to_gar) + (t_gar_to_apt * d_gar_to_apt)
 
-# Toevoegen aan de totale afstand
-total_distance_m = d_service_total_m+d_material_total
-total_distance_km = total_distance_m/1000
+# # Toevoegen aan de totale afstand
+# total_distance_m = d_service_total_m+d_material_total
+# total_distance_km = total_distance_m/1000
 
-print(total_distance_m,total_distance_km,d_material_total)
-
-
-# Bus 85% vol is 300 kwh
+# print(total_distance_m,total_distance_km,d_material_total)
 
 
-# KPI's
+# # Bus 85% vol is 300 kwh
 
-# Berekening aantal bussen
-aantal_ingezette_bussen = bp['bus'].nunique()
 
-# Berekening totale rijafstand: net berekend
+# # KPI's
 
-# Lege/material trips
-t_material_total = t_bst_to_gar + t_gar_to_bst + t_apt_to_gar + t_gar_to_apt + t_apt_to_bst + t_bst_to_apt
+# # Berekening aantal bussen
+# aantal_ingezette_bussen = bp['bus'].nunique()
 
-# Berekening wachttijd
+# # Berekening totale rijafstand: net berekend
 
-# Berekening energiegebruik (Matthijs)
+# # Lege/material trips
+# t_material_total = t_bst_to_gar + t_gar_to_bst + t_apt_to_gar + t_gar_to_apt + t_apt_to_bst + t_bst_to_apt
 
-# Oplaadtijd (Matthijs)
+# # Berekening wachttijd
+
 
 
 
