ec3acb63607afdd533ccd9ecf6adf0a84aa4f52f
MatthijsvanMeeuwen <579720@student.fontys.nl>
2026-09-15 10:52:40 +0200

    Update Code_voor_bus.py

--- Code_voor_bus.py
+++ Code_voor_bus.py
@@ -9,6 +9,17 @@ print(df.head())
 dm = pd.read_excel('DistanceMatrix.xlsx')
 print(dm.head())
 
+total_distance = 0 
+
+start_dis       = dm['start']
+end_dis         = dm['end']
+min_travel_time = dm['min_travel_time']
+max_travel_time = dm['max_travel_time']
+distance_m      = dm['distance_m']
+distance_km     = distance_m/1000
+
+
+
 
 
 
