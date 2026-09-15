386a601f2e5822c3492071bde84af471b0502ff0
BasWensing <559259@student.fontys.nl>
2026-09-15 11:23:11 +0200

    Total distance verder

--- Code_voor_bus.py
+++ Code_voor_bus.py
@@ -119,14 +119,27 @@ distance_km     = distance_m/1000
 
 
 # Bas
+
+
+# Totale afstand van de bussen
 line_400 = dm[dm['line']==400]
 line_401 = dm[dm['line']==401]
-line_400,line_401
+d_ar_to_st400 = line_400['distance_m'].iloc[0]
+d_st_to_ar400 = line_400['distance_m'].iloc[1]
+
+d_ar_to_st401 = line_401['distance_m'].iloc[0]
+d_st_to_ar401 = line_401['distance_m'].iloc[1]
+
+print(d_ar_to_st400,d_st_to_ar400,d_ar_to_st401,d_st_to_ar401)
+
+t_ar_to_st400 = tt[]
+t_st_to_ar400 = tt[]
+t_ar_to_st401 = tt[]
+t_st_to_ar401 = tt[]
 
 
 
-
-
+# Bus 85% vol is 300 kwh
 
 
 
