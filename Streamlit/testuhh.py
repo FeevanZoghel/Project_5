08d7d7adc703d20943ed05c16f4592bc8dc8689b
FeevanZoghel <582755@student.fontys.nl>
2026-09-17 12:50:13 +0200

    Update testuhh.py

--- Streamlit/testuhh.py
+++ Streamlit/testuhh.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 df = pd.read_excel('bus_plan_gecleaned.xlsx')
 print(df.columns)
+
+if df.columns is not ['start location', 'end location', 'start time', 'end time', 'activity',
+       'line', 'energy consumption', 'bus']:
+    print('dinge')
+else:
+    print('Werkt wel!')
