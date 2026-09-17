0579439904b3c142b6c6009bce257bbe94b91955
FeevanZoghel <582755@student.fontys.nl>
2026-09-17 13:00:59 +0200

    Update testuhh.py

--- Streamlit/testuhh.py
+++ Streamlit/testuhh.py
@@ -1,10 +1,21 @@
 import pandas as pd
 
-df = pd.read_excel('bus_plan_gecleaned.xlsx')
-print(df.columns)
+df = pd.read_excel('Bus_Plan_Cleaned.xlsx')
 
-if df.columns is not ['start location', 'end location', 'start time', 'end time', 'activity',
-       'line', 'energy consumption', 'bus']:
-    print('dinge')
-else:
-    print('Werkt wel!')
+
+def Check_columns(df):
+    """
+    
+    Checken of de kolommen uit de gegeven dataset correct zijn
+
+    return: 
+        True als kolommen goed zijn
+        False als kolommen anders zijn
+    
+    miss per kolom checken? dus dan weet je ook welke er mis is
+
+    """
+    columns = df.columns.tolist()
+    good_columns = ['start location', 'end location', 'start time', 'end time', 'activity', 'line', 'energy consumption', 'bus']
+
+    return columns == good_columns
