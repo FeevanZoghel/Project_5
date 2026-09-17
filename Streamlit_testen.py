a06b11d010bf8f3d08ff4eaf8126347823ff691c
FeevanZoghel <582755@student.fontys.nl>
2026-09-17 10:45:24 +0200

    Update Streamlit_testen.py

--- Streamlit_testen.py
+++ Streamlit_testen.py
@@ -5,7 +5,7 @@ st.title("Mijn eerste app")
 st.write("Hallo wereld!")
 
 st.title('Bus planning')
-bus_planning = pd.read_excel('Bus Planning.xlsx')
+bus_planning = pd.read_excel('Bus_Planning.xlsx')
 st.write(bus_planning.head(10))
 
 st.title('Distance Matrix')
@@ -15,4 +15,3 @@ st.write(distance_matrix.head(10))
 st.title('Timetable')
 time_table = pd.read_excel('Timetable.xlsx')
 st.write(time_table.head(10))
-
