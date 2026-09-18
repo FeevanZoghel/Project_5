16046ccd42de1335639f0f3b9038249e4a6bdfcb
FeevanZoghel <582755@student.fontys.nl>
2026-09-18 09:22:54 +0200

    Update Streamlit_app.py

--- Streamlit/Streamlit_app.py
+++ Streamlit/Streamlit_app.py
@@ -5,7 +5,7 @@ from DataFrame_check import check_columns
 from DataFrame_check import only_check_columns
 from DataFrame_check import tijden_check
 
-st.theme.sidebar.primaryColor('blue')
+theme.sidebar.primaryColor('blue')
 
 st.title("Mijn app")
 
