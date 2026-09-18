7a48b69e4995702bc6ea2988628d05cfd5a6f1ff
FeevanZoghel <582755@student.fontys.nl>
2026-09-18 08:29:59 +0200

    Update testuhh.py

--- Streamlit/testuhh.py
+++ Streamlit/testuhh.py
@@ -50,7 +50,7 @@ def only_check_columns(df):
 
     for i in columns:
         if i not in good_columns:
-            st.write('There is an error found in column names')
+            st.error('There is an error found in column names')
             fout = True
             return False
 
